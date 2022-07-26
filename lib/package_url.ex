a991a87e4c27d459c3fa0af611f5e53419fa71db
Udo Schneider <Udo.Schneider@homeaddress.de>
2022-07-26 11:31:04 +0200

    Initial commit

---
+++ lib/package_url.ex
@@ -0,0 +1,18 @@
+defmodule PackageUrl do
+  @moduledoc """
+  Documentation for `PackageUrl`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PackageUrl.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/package_url_test.exs
@@ -0,0 +1,8 @@
+defmodule PackageUrlTest do
+  use ExUnit.Case
+  doctest PackageUrl
+
+  test "greets the world" do
+    assert PackageUrl.hello() == :world
+  end
+end
