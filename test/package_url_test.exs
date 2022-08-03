65f33a35fda6136c8a99fd597cd9c6751af0de8c
Udo Schneider <Udo.Schneider@homeaddress.de>
2022-08-03 17:13:16 +0200

    Added additional test suite tests

--- test/package_url_test.exs
+++ test/package_url_test.exs
@@ -2,19 +2,31 @@ defmodule PackageUrlTest do
   use ExUnit.Case
   doctest PackageUrl
 
-  test_suite_data =
-    File.read!("test/fixtures/test-suite-data.json")
+  test_suite_data_1 =
+    "test/fixtures/test-suite-data-1.json"
+    |> File.read!()
     |> Jason.decode!()
-    |> Enum.map(fn o ->
-      o
-      |> Enum.map(fn {k, v} -> {String.to_atom(k), v} end)
-      |> Enum.into(%{})
+
+  test_suite_data_2 =
+    "test/fixtures/test-suite-data-2.json"
+    |> File.read!()
+    |> Jason.decode!()
+
+  test_suite_data =
+    (test_suite_data_1 ++ test_suite_data_2)
+    |> Enum.map(fn test ->
+      {test["description"],
+       test
+       |> Enum.map(fn {k, v} -> {String.to_atom(k), v} end)
+       |> Enum.into(%{})}
     end)
+    |> Enum.into(%{})
+    |> Map.values()
 
   describe "it should not be possible to create invalid PackageURLs /" do
     for obj <- test_suite_data do
       if obj.is_invalid do
-        test "#{obj.description} / #{inspect(obj.purl)}" do
+        test "#{obj.description}" do
           purl =
             PackageUrl.new(
               type: unquote(obj.type),
@@ -45,7 +57,7 @@ defmodule PackageUrlTest do
   describe "it should not be possible to parse invalid PackageURLs /" do
     for obj <- test_suite_data do
       if obj.is_invalid do
-        test "#{obj.description} / #{inspect(obj.purl)}" do
+        test "#{obj.description}" do
           purl = PackageUrl.new(unquote(obj.purl))
 
           assert match?({:error, _}, purl)
@@ -61,7 +73,7 @@ defmodule PackageUrlTest do
   describe "it should be able to create valid PackageURLs /" do
     for obj <- test_suite_data do
       unless obj.is_invalid do
-        test "#{obj.description} / #{inspect(obj.purl)}" do
+        test "#{obj.description}" do
           {:ok, purl} =
             PackageUrl.new(
               type: unquote(obj.type),
@@ -86,7 +98,7 @@ defmodule PackageUrlTest do
   describe "should be able to convert valid PackageURLs to a string /" do
     for obj <- test_suite_data do
       unless obj.is_invalid do
-        test "#{obj.description} / #{inspect(obj.purl)}" do
+        test "#{obj.description}" do
           {:ok, purl} =
             PackageUrl.new(
               type: unquote(obj.type),
@@ -106,7 +118,7 @@ defmodule PackageUrlTest do
   describe "it should be able to parse valid PackageURLs /" do
     for obj <- test_suite_data do
       unless obj.is_invalid do
-        test "#{obj.description} / #{inspect(obj.purl)}" do
+        test "#{obj.description}" do
           {:ok, purl} = PackageUrl.new(unquote(obj.canonical_purl))
 
           assert PackageUrl.to_string!(purl) == unquote(obj.canonical_purl)
@@ -116,6 +128,87 @@ defmodule PackageUrlTest do
           assert purl.version == unquote(obj.version)
           assert purl.qualifiers == unquote(Macro.escape(obj.qualifiers))
           assert purl.subpath == unquote(obj.subpath)
+        end
+      end
+    end
+  end
+
+  describe "parsing the test canonical purl then re-building a purl from these parsed components should return the test canonical purl /" do
+    for obj <- test_suite_data do
+      unless obj.is_invalid do
+        test "#{obj.description}" do
+          {:ok, purl} = PackageUrl.new(unquote(obj.canonical_purl))
+
+          assert PackageUrl.to_string!(purl) == unquote(obj.canonical_purl)
+        end
+      end
+    end
+  end
+
+  describe "parsing the test purl should return the components parsed from the test canonical purl /" do
+    for obj <- test_suite_data do
+      unless obj.is_invalid do
+        test "#{obj.description}" do
+          purl = PackageUrl.new!(unquote(obj.purl))
+          canonical_purl = PackageUrl.new!(unquote(obj.canonical_purl))
+
+          unless purl.namespace == canonical_purl.namespace do
+            IO.inspect(unquote(obj.purl))
+            IO.inspect(purl)
+            IO.inspect(unquote(obj.canonical_purl))
+            IO.inspect(canonical_purl)
+          end
+
+          assert purl.type == canonical_purl.type
+          assert purl.namespace == canonical_purl.namespace
+          assert purl.name == canonical_purl.name
+          assert purl.version == canonical_purl.version
+          assert purl.qualifiers == canonical_purl.qualifiers
+          assert purl.subpath == canonical_purl.subpath
+        end
+      end
+    end
+  end
+
+  describe "parsing the test purl then re-building a purl from these parsed components should return the test canonical purl /" do
+    for obj <- test_suite_data do
+      unless obj.is_invalid do
+        test "#{obj.description}" do
+          purl = PackageUrl.new!(unquote(obj.purl))
+
+          rebuilt =
+            PackageUrl.new!(
+              type: purl.type,
+              namespace: purl.namespace,
+              name: purl.name,
+              version: purl.version,
+              qualifiers: purl.qualifiers,
+              subpath: purl.subpath
+            )
+
+          assert PackageUrl.to_string!(rebuilt) == unquote(obj.canonical_purl)
+        end
+      end
+    end
+  end
+
+  describe "building a purl from the test components should return the test canonical purl /" do
+    for obj <- test_suite_data do
+      unless obj.is_invalid do
+        test "#{obj.description}" do
+          purl =
+            PackageUrl.new!(
+              type: unquote(obj.type),
+              namespace: unquote(obj.namespace),
+              name: unquote(obj.name),
+              version: unquote(obj.version),
+              qualifiers: unquote(Macro.escape(obj.qualifiers)),
+              subpath: unquote(obj.subpath)
+            )
+
+          canonical_purl = PackageUrl.new!(unquote(obj.canonical_purl))
+
+          assert purl == canonical_purl
         end
       end
     end
